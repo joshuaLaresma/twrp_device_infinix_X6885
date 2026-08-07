10943abadaa109e115358614ce5fb161c66e421e
joshuaLaresma <158560563+joshuaLaresma@users.noreply.github.com>
2026-08-07 16:25:00 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,6 +174,3 @@ PB_MAINTAINER := "アンドレイ"
 
 # SELinux Configuration
 BOARD_SEPOLICY_DIRS += device/infinix/X6885/sepolicy
-
-# Recovery Root Overlay
-TARGET_RECOVERY_ROOT_OUT := device/infinix/X6885/recovery/root/system/usr/idc/
